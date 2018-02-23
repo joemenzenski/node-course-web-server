33f39a1c7e9fdaa3d83ae90747b3cd6ba38ecdb5
Joey Menzenski <jmenzenski@mitre.org>
2018-02-23 15:29:49 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+
+const express = require('express');
+const hbs = require('hbs');
+
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+// simple logging middleware
+app.use((request, response, next) => {
+
+    const now = new Date().toString();
+    const log = `${now}: ${request.method} ${request.url}`;
+    console.log(log);
+
+    fs.appendFile('server.log', log + '\n', (error) => {
+        if (error) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+    
+});
+
+// app.use((request, response, next) => {
+//     response.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear() );
+hbs.registerHelper('screamIt', (text) => text.toUpperCase() );
+
+app.get('/', (request, response) => {
+    
+    response.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome!!!'
+    });
+
+});
+
+app.get('/about', (request, response) => {
+    
+    response.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+
+});
+
+app.get('/bad', (request, response) => {
+    response.send({
+        errorMessage: 'This is an error'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000!');
+});
